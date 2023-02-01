f1c47b7f6c2cbf833b9682d700d9884ee4bcbbac
Rubel Rudra <rubelrudra27@gmail.com>
2023-02-01 11:12:34 +0600

    loop in array

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+// display array element using for loop
+var learn=['HTML', 'Vanila CSS', 'Bootstrap', 'Tailwind CSS', 'Basic Javascript'];
+for(var i=0;i<learn.length;i++){
+    console.log(i,'.',learn[i]);
+}
+
+// display array element using while loop
+
+var phoneBrand=['Nokia', 'Techno', 'Walton', 'Symphony', 'Xiaomi'],i=0;
+while(i<phoneBrand.length){
+    console.log(phoneBrand[i]);
+    i++;
+}
