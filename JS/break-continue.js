807da27221a414a1014d8f33978330246a80eaf8
Rubel Rudra <rubelrudra27@gmail.com>
2023-02-01 11:44:02 +0600

    learn about break/continue statement

--- JS/break-continue.js
+++ JS/break-continue.js
@@ -5,3 +5,13 @@ for(var i=30;i<=86;i++){
     }
     console.log(i);
 }
+
+// continue statement
+
+var bookPrice=[200,120,350,150,180,300,100,250];
+for(i=0;i<bookPrice.length;i++){
+    if(bookPrice[i]>=200){
+        continue;
+    }
+    console.log(bookPrice[i]);
+}
