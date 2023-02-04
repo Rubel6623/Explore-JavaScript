a27fe64c65cdc829e15c5d5e4292db403933990e
Rubel Rudra <rubelrudra27@gmail.com>
2023-02-05 00:08:38 +0600

    to lower case

---
+++ JS/toLowercase.js
@@ -0,0 +1,9 @@
+let username='blackPink';
+let userInput='Blackpink';
+console.log(userInput.toUpperCase());
+if(username.toLowerCase()=== userInput.toLowerCase()){
+    console.log('Valid User');
+}
+else{
+    console.log('Invalid user');
+}  
